552e7879a98071eb68a8fa1f04bb7c421ab22a17
atajmouti <atajmouti@efrontech.com>
2020-06-23 10:28:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+
+interface Campagne {
+  Id: string;
+  name: string;
+}
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'ng-gtmt';
+
+
+  campagnes: Campagne[] = [
+    {Id: '1', name: 'Campagne courrier 1'},
+    {Id: '2', name: 'Campagne courrier 2'},
+    {Id: '3', name: 'Campagne courrier 3'},
+    {Id: '4', name: 'Campagne courrier 4'},
+    {Id: '5', name: 'Campagne courrier 5'},
+  ];
+
+  template = new FormControl();
+
+   days = ['Lundi', 'Mardi','Mecredi', 'Jeud', 'Vendredi', 'Samedi'];
+
+
+  isLinear: true;
+  form: FormGroup;
+
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer,
+    private domSanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon(
+      'logolp_part',
+      sanitizer.bypassSecurityTrustResourceUrl('./assets/logolp_part.svg'));
+  }
+
+  ngOnInit() {
+    this.form = this._formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      frequence: ['', Validators.required],
+      frequencenature: [''], // weekly or monthly
+      startdate: [''],
+      enddate: [''],
+      weekday: [''],
+      monthday: [''],
+      EndDate: ['']    
+    });
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,52 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    MatDatepickerModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatStepperModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatNativeDateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
